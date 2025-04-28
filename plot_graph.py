deec6feb6c29de990bd525b7016f10569fb29063
Erikaange <akengneheukoua@student.cscc.edu>
2025-04-28 17:03:22 -0400

    added graph title and some background color

--- plot_graph.py
+++ plot_graph.py
@@ -5,11 +5,12 @@ from build_graph import DataSet
 
 form_graph = DataSet(a=1, b=-3, c=4)
 x,y = form_graph.quad_formula()
-plt.style.use('bmh')
+plt.style.use('classic')
 # Create a new figure and axis for the plot
 figure, graph = plt.subplots()
 
 graph.plot(x,y)
+graph.set_title(f'Quadratic formula graph')
 
 
 graph.grid(True)
