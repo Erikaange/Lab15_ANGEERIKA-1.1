5d9f7c3e6899e30d998dd64852e853fc57d232e3
Erikaange <akengneheukoua@student.cscc.edu>
2025-04-28 16:57:31 -0400

    refactor plotting to use axis object from subplots

--- plot_graph.py
+++ plot_graph.py
@@ -5,8 +5,12 @@ from build_graph import DataSet
 
 form_graph = DataSet(a=1, b=-3, c=4)
 x,y = form_graph.quad_formula()
-plt.plot(x,y)
+plt.style.use('bmh')
+# Create a new figure and axis for the plot
+figure, graph = plt.subplots()
+
+graph.plot(x,y)
 
 
-plt.grid(True)
+graph.grid(True)
 plt.show()
